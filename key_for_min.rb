3ca16be0d5e551324f71416879a8f2b6d7d6044d
Yu Hao (Tom) Lai <tomlai8882000@hotmail.com>
2020-07-04 04:18:15 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,20 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+  
+  youngest = nil
+  value = 999
+  name_hash.each do |name, age|
+    if value > age
+      value = age
+      
+      youngest = name
+      
+    end 
+  end
+    
+  youngest
+      
+  
 
 end
